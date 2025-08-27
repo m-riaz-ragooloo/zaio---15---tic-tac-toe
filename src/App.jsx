c1cfa18f01ecfc77dbd65db79bed8350e7209a83
Riaz Ragooloo <riazragooloo@gmail.com>
2025-08-27 15:57:53 +0200

    added theme contect code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import Router from "./Router"
+import { useContext } from "react";
+import Router from "./Router";
+import { GlobalStyle } from "./styles/Global.styled";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./styles/theme";
+import { ThemeContext } from "./contexts/ThemeContext";
 
 export default function App() {
+  const {theme} = useContext(ThemeContext)
+
+  const mode = (theme === "light" ? lightTheme : darkTheme);
+
   return (
-    <Router />
-  )
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
+  );
 }
