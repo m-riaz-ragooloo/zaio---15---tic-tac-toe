0f956daf758de63b6ef1fde7fef5ed2d00aed63d
Riaz Ragooloo <riazragooloo@gmail.com>
2025-08-28 11:22:43 +0200

    added global styling and base theme base code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from "./styles/Global.styled";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./styles/theme";
 import { ThemeContext } from "./contexts/ThemeContext";
+import ThemeContextProvider from "./contexts/ThemeContextProvider";
 
 export default function App() {
   const {theme} = useContext(ThemeContext)
@@ -11,7 +12,7 @@ export default function App() {
   const mode = (theme === "light" ? lightTheme : darkTheme);
 
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={mode}>
       <GlobalStyle />
       <Router />
     </ThemeProvider>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Provider from "./contexts/Provider";
+import React from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <Provider>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
